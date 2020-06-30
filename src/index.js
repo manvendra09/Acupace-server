e91e3b4b1161ccf125ecc5b946c337c2144f762c
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-07-01 01:00:37 +0530

    Chat Service server

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+
+const http = require('http');
+const server = http.Server(app);
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('new-message', (message) => {
+        console.log(message);
+        io.emit('newmessage',message);
+    });
+});
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
