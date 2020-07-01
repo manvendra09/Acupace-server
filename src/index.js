879919915f5ffa2788f3873e292aeb4b48d95cb2
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-07-01 16:28:36 +0530

    Chat service enhanced with the arguments

--- src/index.js
+++ src/index.js
@@ -6,14 +6,26 @@ const server = http.Server(app);
 
 const socketIO = require('socket.io');
 const io = socketIO(server);
-
+var {User}=require('./Userclass');
 const port = process.env.PORT || 3000;
-
+var users=new User();
 io.on('connection', (socket) => {
+    let channel_name=socket.handshake.query.channel_name;
+    let user_name=socket.handshake.query.user_name;
     console.log('user connected');
+    //Joining the particular Channel
+    socket.join(channel_name);
+    users.Addusername(socket.id,user_name,channel_name);
     socket.on('new-message', (message) => {
         console.log(message);
         io.emit('newmessage',message);
+    });
+    // Remove on disconnect
+    socket.on("disconnect",function(){
+        var user=users.RemoveUser(socket.id);
+        if(user){
+            io.to(user.room).emit("namelist",users.GetUserList(user.room));
+        }
     });
 });
 server.listen(port, () => {
